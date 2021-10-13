65547f9e6f018e76b77b4ff416097d6fb72c9a3e
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-13 19:56:26 +0900

    Update ani.py

--- ani.py
+++ ani.py
@@ -3,6 +3,7 @@ st.title("アニバーサリー総選挙2021 予想")
 st.button('再予想')
 seigen=st.checkbox('制限を解除する')
 simulation=st.radio("シミュレーションしますか？",("しない","30連でシミュレーション","60連でシミュレーション"))
+toba=st.text_input('', '野﨑康誠')
 
 import numpy as np
 
