3d4cfabb51a0690860120fa36264e816e45d98ae
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-24 00:54:18 +0900

    Update ani.py

--- ani.py
+++ ani.py
@@ -3,9 +3,9 @@ import streamlit as st
 def main():
     status_area = st.empty()
     count_down_sec = 5
-        for i in range(count_down_sec):
-                status_area.write(f'{count_down_sec - i} sec left')
-                time.sleep(1)
+    for i in range(count_down_sec):
+        status_area.write(f'{count_down_sec - i} sec left')
+        time.sleep(1)
 st.title("アニバーサリー総選挙2021 予想")
 simulation=st.radio("何をシミュレーションしますか？",("60連目をシミュレーション","累計報酬をシミュレーション"))
 toba=st.text_input('名前を入力してください', '調子くん')
