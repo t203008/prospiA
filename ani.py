f4c6f223632cbc94ad710e05513bbd14e6020b94
t203008 <91816208+t203008@users.noreply.github.com>
2021-10-24 00:53:47 +0900

    Update ani.py

--- ani.py
+++ ani.py
@@ -2,7 +2,7 @@ import time
 import streamlit as st
 def main():
     status_area = st.empty()
-        count_down_sec = 5
+    count_down_sec = 5
         for i in range(count_down_sec):
                 status_area.write(f'{count_down_sec - i} sec left')
                 time.sleep(1)
